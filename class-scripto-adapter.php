de4c1f49eda5b93d55f1b3f7de667fccfd8e4847
Jim Safley <jimsafley@gmail.com>
2011-12-14 16:40:16 -0500

    Update Scripto library. Remove preprocessor limit reports.

--- class-scripto-adapter.php
+++ class-scripto-adapter.php
@@ -132,6 +132,7 @@ class Scripto_Adapter implements Scripto_Adapter_Interface {
 	 * Import the attachment post transcription.
 	 */
 	public function importDocumentPageTranscription( $post_id, $attachment_post_id, $text ) {
+		$text = Scripto::removeNewPPLimitReports( $text );
 		update_post_meta( $attachment_post_id, 'scripto_attachment_transcription', $text );
 	}
 	
@@ -140,6 +141,7 @@ class Scripto_Adapter implements Scripto_Adapter_Interface {
 	 * post.
 	 */
 	public function importDocumentTranscription( $post_id, $text ) {
+		$text = Scripto::removeNewPPLimitReports( $text );
 		update_post_meta( $post_id, 'scripto_post_transcription', $text );
 	}
 }
